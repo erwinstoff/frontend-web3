88eebd853db5338475a95fc68abb9b799e676f75
erwinstoff <erwinstoff8212@gmail.com>
2025-08-29 17:52:58 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    turbo: {
+      resolveAlias: {
+        "pino-pretty": false, // ⬅️ exclude from browser bundle
+      },
+    },
+  },
 };
 
 export default nextConfig;
