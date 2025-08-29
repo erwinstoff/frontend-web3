0dac21edf5bd804bb771be2f5176791d0a38d87a
erwinstoff <erwinstoff8212@gmail.com>
2025-08-29 18:40:04 +0000

    update changes made in project

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,10 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
-  // Exclude modules in Webpack
-  webpack: (config) => {
-    config.externals?.push("pino-pretty", "lokijs", "encoding");
-    return config;
-  },
-
-  // Exclude in Turbopack by aliasing to mock
-  experimental: {
-    turbo: {
-      resolveAlias: {
-        "pino-pretty": path.resolve(__dirname, "./mock.js"),
-        "lokijs": path.resolve(__dirname, "./mock.js"),
-        "encoding": path.resolve(__dirname, "./mock.js"),
-      },
-    },
-  },
-
-  // Ensure server-only modules aren’t bundled for the client
-  serverExternalPackages: ['pino-pretty', 'encoding'],
+  webpack: config => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding')
+    return config
+  }
 };
 
 export default nextConfig;
