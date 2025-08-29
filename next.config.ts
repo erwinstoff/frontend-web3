848348c021d9348daba700789f5e8d5d6794b9ac
erwinstoff <erwinstoff8212@gmail.com>
2025-08-29 17:21:20 +0000

    save all updates

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  
-const nextConfig = {
   webpack: (config) => {
-    config.externals.push("pino-pretty", "lokijs", "encoding");
+  config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
 };
